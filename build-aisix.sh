a7cd7e591129373af8b64291bac1c53ede283f1a
hyenasky <ashnwill49@gmail.com>
2019-09-24 00:07:58 -0600

    update build scripts

--- build-aisix.sh
+++ build-aisix.sh
@@ -4,6 +4,6 @@ path=$(dirname $0)
 
 mkdir -p ${path}/tmp/
 
-./sdk/dragonc-flat.sh ${path}/src/sys/kernel/Aisix.d ${path}/tmp/aisix
+./sdk/dragonc.sh -flat ${path}/src/sys/kernel/Aisix.d ${path}/tmp/aisix
 
 ./sdk/fsutil.sh $1 w /aisix ${path}/tmp/aisix
