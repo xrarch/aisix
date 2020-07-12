4642867fbffe8a3a3773af1b2bd2cefec149fa52
hyenasky <ashnwill49@gmail.com>
2020-07-12 16:52:54 -0600

    update standalone utilities and bootloader to new abi, kernel is now broken though

--- src/stand/diag/citron.h
+++ src/stand/diag/citron.h
@@ -1,18 +1,18 @@
 const DCitronBase 0xF8000000
 
-extern DCitronInb (* port -- byte *)
+extern DCitronInb { port -- byte }
 
-extern DCitronIni (* port -- int *)
+extern DCitronIni { port -- int }
 
-extern DCitronInl (* port -- long *)
+extern DCitronInl { port -- long }
 
-extern DCitronOutb (* byte port -- *)
+extern DCitronOutb { byte port -- }
 
-extern DCitronOuti (* int port -- *)
+extern DCitronOuti { int port -- }
 
-extern DCitronOutl (* long port -- *)
+extern DCitronOutl { long port -- }
 
-extern DCitronCommand (* command port -- *)
+extern DCitronCommand { command port -- }
 
 (* doesn't wait for the device to report the operation as completed before returning *)
-extern DCitronCommandASync (* command port -- *)
+extern DCitronCommandASync { command port -- }
